6982f366bcfc4f90029a8bf04cde388a363a8523
sumesh <sumesh@frontlinetech.in>
2026-07-04 16:31:51 +0530

    added

--- src/components/home/contact.jsx
+++ src/components/home/contact.jsx
@@ -66,10 +66,20 @@ const Contact = () => {
       </div>
 
       <div className="contact-map">
-        <iframe
+        {/* <iframe
           title="location"
           src="https://www.google.com/maps?q=Chennai&output=embed"
           loading="lazy"
+        ></iframe> */}
+        <iframe
+          title="location"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1784590.9217407145!2d76.12152582400076!3d11.307327386312982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae0d1a06b2d8f7%3A0x395a5643676b4f41!2sBangalore%20Cement%20Pipes%20RCC%20Hume%20pipes!5e0!3m2!1sen!2sin!4v1783162841491!5m2!1sen!2sin"
+          width="600"
+          height="450"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="strict-origin-when-cross-origin"
         ></iframe>
       </div>
     </section>
